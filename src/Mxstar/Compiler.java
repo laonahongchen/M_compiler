8ae545cffc58f14e8453bff9b0359f241545178c
laonahongchen <chenweizhe1999@126.com>
2019-05-05 18:40:13 +0800

    changed input source

--- src/Mxstar/Compiler.java
+++ src/Mxstar/Compiler.java
@@ -24,9 +24,9 @@ public class Compiler {
         exit(0);
     }
     public static void compile() throws IOException{
-//        BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
         //InputStream is = Config.in;
-        FileInputStream is = new FileInputStream("program.txt");
+//        FileInputStream is = new FileInputStream("program.txt");
         ANTLRInputStream ais = new ANTLRInputStream(is);
         MxstarLexer mstarLexer = new MxstarLexer(ais);
         CommonTokenStream tokens = new CommonTokenStream(mstarLexer);
